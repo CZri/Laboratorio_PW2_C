e1e540486e9ff30f18dc00a03bd2e5fd91e82949
CZri <rcondoz@unsa.edu.pe>
2023-07-21 10:34:27 -0500

    no me deja push

--- TareaProject/TareaProject/urls.py
+++ TareaProject/TareaProject/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('./TareaApp/App.urls')),
+    path('',include('App.urls')),
 ]
 
    
